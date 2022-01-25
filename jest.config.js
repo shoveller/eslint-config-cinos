6bc370946fbdf6a12aade68a5c2cf964f1441da5
fp91224 <fp91224@linecorp.com>
2022-01-25 16:33:06 +0900

    feat: 의존성을 주입할 수있게 구현을 수정

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@
 const config = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|mjs?)$',
   transform: {
-    '^.+\\.mjs$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleFileExtensions: ['js', 'mjs'],
--- release.js
+++ release.js
@@ -1,29 +1,25 @@
 /**
- * https://cheatcode.co/tutorials/how-to-write-test-and-publish-an-npm-package#writing-a-release-script
+ * Https://cheatcode.co/tutorials/how-to-write-test-and-publish-an-npm-package#writing-a-release-script.
  */
-/* @type {import('node')}*/
+/** @type {import('node')}*/
 import {execSync} from 'child_process'
 import fs from 'fs'
-/* @type {import('semver')}*/
+/** @type {import('semver')}*/
 import semver from 'semver'
 
 export const getPackageJSON = () => {
-  const result = fs.readFileSync(packageJSONPath, 'utf-8')
+  const result = fs.readFileSync(getPackageJSONPath(), 'utf-8')
 
   return JSON.parse(result)
 }
 
-/**
- *
- * @param {Object} content
- */
 export const setPackageJSON = (content) => {
-  fs.writeFileSync(packageJSONPath, JSON.stringify(content, null, 2))
+  fs.writeFileSync(getPackageJSONPath(), JSON.stringify(content, null, 2))
 }
 
-export const packageJSONPath = './package.json'
+export const getPackageJSONPath = () => './package.json'
 
-export const isDev = process.env.NODE_ENV === 'development'
+export const isDevEnv = () => process.env.NODE_ENV === 'development'
 
 export const getOriginalVersion = () => {
   const packageJSON = getPackageJSON()
@@ -31,42 +27,25 @@ export const getOriginalVersion = () => {
   return `${packageJSON.version}`
 }
 
-/**
- * 새 버전을 계산한다
- * 개발 모드일때는 developmentVersion 필드의 마이너 버전을 늘린 값을 반환한다
- * 그 외에는 version 필드의 마이너 버전을 늘린 값을 반환한다
- * @returns {string}
- */
 export const getNewVersion = () => {
   const { developmentVersion = '', version = '' } = getPackageJSON()
-  if (isDev) {
+  if (isDevEnv()) {
     return semver.inc(developmentVersion,'minor');
   }
 
   return semver.inc(version,'minor');
 }
 
-/**
- * 개발 모드에서는 강제로 publish 한다
- */
-const getForceFlag = () => isDev ? '--force' : ''
+const getForceFlag = () => isDevEnv() ? '--force' : ''
 
-/**
- *
- * @returns {string}
- */
 const getRegistry = () => {
-  if (isDev) {
+  if (isDevEnv()) {
     return '--registry http://localhost:4873'
   }
 
   return '';
 }
 
-/**
- *
- * @return {string}
- */
 export const getVersionUpCmd = () => {
   const newVersion = getNewVersion()
   const registry = getRegistry()
@@ -74,10 +53,6 @@ export const getVersionUpCmd = () => {
   return `npm version ${newVersion} --allow-same-version ${registry}`
 }
 
-/**
- *
- * @return {string}
- */
 export const getPublishCmd = () => {
   const registry = getRegistry()
   const forceFlag = getForceFlag()
@@ -85,10 +60,6 @@ export const getPublishCmd = () => {
   return `npm publish --access public ${forceFlag} ${registry}`
 }
 
-/**
- *
- * @returns {`${string} && ${string}`}
- */
 export const getCommand = () => {
   const versionUpCmd = getVersionUpCmd()
   const publishCmd = getPublishCmd()
@@ -119,8 +90,8 @@ try {
 }
 
 /**
- * 개발모드로 배포했다면, 배포가 성공하더라도 버전을 롤백한다
+ * 개발모드로 배포했다면, 배포가 성공하더라도 버전을 롤백한다.
  */
-if (isDev) {
+if (isDevEnv()) {
   versionRollback()
 }
